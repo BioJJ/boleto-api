ff22a96aed56245e65d520f39914682bb069d472
Jefferson Coelho 🐱‍👤 <bio.jeffcoelho@gmail.com>
2022-02-06 13:00:36 -0400

    ✨test controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoletoModule } from './boleto/boleto.module';
 
 @Module({
-  imports: [],
+  imports: [BoletoModule],
   controllers: [AppController],
   providers: [AppService],
 })
